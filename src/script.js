163204bdc8e3e692e4cc1416d32b503aed48dd59
Gr0biX <christian.erb@swk.de>
2022-11-23 09:55:18 +0100

    sfdsdf

--- src/script.js
+++ src/script.js
@@ -11,6 +11,6 @@ function httpGet(theUrl) {
   var entries = vehicle["EntryCount"]
   var exits = vehicle["ExitCount"]
   console.log(vehicle)
-  document.querySelector('#1').innerHTML = vehicles
-  document.querySelector('#2').innerHTML = entries
-  document.querySelector('#3').innerHTML = exits
+  document.querySelector('#Test1').innerHTML = vehicles
+  document.querySelector('#Test2').innerHTML = entries
+  document.querySelector('#Test3').innerHTML = exits
